dee83e14aa11808a32493aac32074fea03c98948
Viktor 40in <zambono@yandex.ru>
2020-11-15 14:15:35 +0300

    mesto0.6

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,14 @@ let editButton = document.querySelector('.profile-info__edit-button');
 let popupCloseButton = document.querySelector('.popup__close');
 let form = document.querySelector('.popup__form');
 let title = document.querySelector('.profile-info__title');
-let subtitle = document.querySelector('.profile-info__subtitle');
+let subTitle = document.querySelector('.profile-info__subtitle');
 let fieldOne = document.querySelector('.popup__input_type-name');
 let fieldTwo = document.querySelector('.popup__input_type-profession');
 
 function doPopup() {
   popup.classList.toggle('popup_opened');
+  fieldOne.value = title.textContent;
+  fieldTwo.value = subTitle.textContent;
 }
 
 function popupClickHandler(event) {
@@ -16,12 +18,12 @@ function popupClickHandler(event) {
     doPopup();
   }
 }
-
+  
 function submitForm(event) {
   event.preventDefault();
   title.textContent = fieldOne.value;
-  subtitle.textContent = fieldTwo.value;
-  doPopup()
+  subTitle.textContent = fieldTwo.value;
+  doPopup();
 }
 
 editButton.addEventListener('click', doPopup);
